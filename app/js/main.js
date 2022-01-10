a27ee3ea717774f650726583df8d6c063f0d873b
spjustice <sanyok.vergun@gmail.com>
2022-01-10 22:26:09 +0300

    page reviews

--- app/js/main.js
+++ app/js/main.js
@@ -726,3 +726,72 @@ $('.blog-sidebar-dropd-large__icon').on('click', function () {
 $('.blog-sidebar-dropd-small__icon').on('click', function () {
    $(this).closest('.blog-sidebar-dropd-small').toggleClass('open');
 })
+
+// stars
+$('.js-star-item').mouseover(function () {
+   count = 0;
+   var i = 0;
+   var j = 0;
+   var thisParent = $(this).closest('.js-star-block');
+   var thisItem = thisParent.find('.js-star-item');
+
+   $(thisItem).each(function () {
+      if ($(this).hasClass('active')) {
+         count++;
+      }
+   })
+   $(thisItem).each(function () {
+      $(this).removeClass('active');
+   })
+   $(this).addClass('js-prov');
+   $(thisItem).each(function () {
+      $(this).removeClass('js-hover');
+   })
+   $(thisItem).each(function (index) {
+      if ($(this).hasClass('js-prov')) {
+         i = index;
+      }
+   })
+   $(thisItem).each(function (index) {
+      if (index < i + 1) {
+         $(this).addClass('js-hover');
+      }
+   })
+})
+$('.js-star-item').mouseout(function () {
+   var thisParent = $(this).closest('.js-star-block');
+   var thisItem = thisParent.find('.js-star-item');
+
+   $(this).removeClass('js-prov');
+   $(thisItem).each(function () {
+      $(this).removeClass('js-hover');
+   })
+   $(thisItem).each(function (index) {
+      if (index < count) {
+         $(this).addClass('active');
+      }
+   })
+})
+$('.js-star-item').on('click', function () {
+   var count_star = 0;
+   count = 0;
+   var i = 0;
+   var thisParent = $(this).closest('.js-star-block');
+   var thisItem = thisParent.find('.js-star-item');
+   $(this).addClass('active');
+   $(thisItem).each(function (index) {
+      if ($(this).hasClass('active')) {
+         i = index;
+      }
+   })
+   $(thisItem).each(function () {
+      $(this).removeClass('active');
+   })
+   $(thisItem).each(function (index) {
+      if (index < i + 1) {
+         $(this).addClass('active');
+         count_star++;
+      }
+   })
+});
+// stars end
